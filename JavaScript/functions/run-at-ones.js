a33c8d5be7e0d51e02b05679b17c7fe190e75bb3
Shankar Balaji <72439750+funnyguy369@users.noreply.github.com>
2023-02-11 22:37:12 +0530

    Create run-at-ones.js

---
+++ JavaScript/functions/run-at-ones.js
@@ -0,0 +1,17 @@
+function main() {
+	console.log("Hello");
+}
+const CALLER_TIMEOUT = 5000;  			// 5 seconds
+function run_ones() {
+    if (is_called) {
+        is_called = false;
+        setTimeout(() => {
+            main();
+            is_called = true;
+        }, CALLER_TIMEOUT);
+    }
+}
+
+for (int i=0; i < 10; i++) {
+	run_ones();
+}
