f8232d092ea0f18d61518e2ebd39da6897dc9a75
Shankar Balaji <72439750+Shankar-Balaji-U@users.noreply.github.com>
2024-05-12 00:16:49 +0530

    Update stopwatch.js
    Fixed the stop function if condition.

--- JavaScript/functions/stopwatch.js
+++ JavaScript/functions/stopwatch.js
@@ -30,8 +30,8 @@ class StopWatch extends EventTarget {
     _runTimer() {
         var $this = this;
         this.timerID = setInterval(function() {
-            $this.addTime($this.interval);
-        }, this.interval);
+                $this.addTime($this.interval);
+            }, this.interval);
     }
 
     start() {
@@ -44,7 +44,7 @@ class StopWatch extends EventTarget {
     }
 
     pause() {
-        if (this.state !== this.STATE.Played)
+                if (!([this.STATE.Played, this.STATE.Started].includes(this.state))) // not Played or Started
             return;
         clearInterval(this.timerID);
         this.state = this.STATE.Paused;
@@ -60,7 +60,7 @@ class StopWatch extends EventTarget {
     }
 
     stop() {
-        if (this.state !== this.STATE.Started)
+        if (this.state === this.STATE.Stopped)
             return;
         this.toTime = new Date();
         clearInterval(this.timerID);
